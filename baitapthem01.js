c381e57135e967d14724716be2cf235e8a99aef8
lehoangtrucmai <125021302+lehoangtrucmai@users.noreply.github.com>
2023-02-25 13:54:12 +0900

    Add files via upload

---
+++ baitapthem01.js
@@ -0,0 +1,18 @@
+let masinhvien;
+masinhvien = prompt("Nhập vào mã sinh viên");
+
+let tensinhvien;
+tensinhvien = prompt("Nhập vào tên sinh viên");
+
+let tuoi;
+tuoi = prompt("Nhập vào tuổi",Number);
+
+let gioitinh ;
+gioitinh =prompt("Nhập vào giơi tính",Boolean);
+
+let diachi;
+diachi = prompt("Nhập vào địa chỉ");
+alert("Địa chỉ sinh viên",diachi);
+
+let diemTB = 9.8;
+document.write("Diierm Trung bình",diemTB);
